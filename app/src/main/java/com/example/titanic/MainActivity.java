dfd7b8bc67ba597626fcb6144b6ed3081ad5e27a
zatout <zatoutredha@gmail.com>
2020-10-05 00:28:47 +0100

    rien a dire

--- app/src/main/java/com/example/titanic/MainActivity.java
+++ app/src/main/java/com/example/titanic/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.titanic;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cursoradapter.widget.SimpleCursorAdapter;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FilterQueryProvider;
 import android.widget.ListView;
@@ -18,12 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
     private MedecinDbAdapter dbHelper;
     private SimpleCursorAdapter dataAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dbHelper = new MedecinDbAdapter(this);
         dbHelper.open();
+
 
         //Clean all data
         dbHelper.deleteAllMedecins();
--- app/src/main/java/com/example/titanic/MedecinDbAdapter.java
+++ app/src/main/java/com/example/titanic/MedecinDbAdapter.java
@@ -131,6 +131,30 @@ public class MedecinDbAdapter {
 
         createMedecin("0664864468","Karina","Pediatre","Chemin des mousseaux ,Vigneux Sur seine ");
         createMedecin("0762017599","Redha","Cardiologue","Alger Centre ");
+        createMedecin("0771850125","Mehdi","Ophtalmologue","Oran ");
+        createMedecin("0771680088","Youcef","Gynécologue","Setif ");
+        createMedecin("0553521197","Tahar","Orthopédiste","Annaba");
+        createMedecin("0662062187","Leila","Generaliste","Alger Centre ");
+        createMedecin("0663954126","Amine","Dentiste","Alger Centre ");
+        createMedecin("0754169853","Nesrine","Ophtalmologue","Alger Centre ");
+        createMedecin("0123569874","Sami","Cardiologue","Alger Centre ");
+        createMedecin("0123654789","Tamila","Pediatre","Alger Centre ");
+        createMedecin("0762017599","Neil","Cardiologue","Alger Centre ");
+        createMedecin("0762017599","Jenna","Ophtalmologue","Alger Centre ");
+        createMedecin("0762017599","Mohamed","Cardiologue","Alger Centre ");
+        createMedecin("0762017599","Nadia","Cardiologue","Alger Centre ");
+        createMedecin("0762017599","Fares","Ophtalmologue","Alger Centre ");
+        createMedecin("0762017599","Ilyas","Pediatre","Alger Centre ");
+        createMedecin("0762017599","Sofiane","Cardiologue","Alger Centre ");
+        createMedecin("0762017599","Anis","Generaliste","Alger Centre ");
+        createMedecin("0762017599","Rabah","Orthopédiste","Alger Centre ");
+        createMedecin("0762017599","Mohand","Generaliste","Alger Centre ");
+        createMedecin("0762017599","Salim","Cardiologue","Alger Centre ");
+        createMedecin("0762017599","Fouzi","Pneumologue","Alger Centre ");
+        createMedecin("0762017599","Said","Dentiste","Alger Centre ");
+
+
+
 
     }
 }
